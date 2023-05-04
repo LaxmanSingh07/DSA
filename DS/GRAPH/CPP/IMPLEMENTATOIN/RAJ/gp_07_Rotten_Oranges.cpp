6013804cfb4e95d539906f08ab2ce72f3ba45c12
LAXMAN <laxsingh9659@gmail.com>
2023-05-04 06:16:47 +0530

    Graph Questions

---
+++ DS/GRAPH/CPP/IMPLEMENTATOIN/RAJ/gp_07_Rotten_Oranges.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        int cntFresh=0;
+        int cnt=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        
+        int maxi=-1e8;
+        
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+                else if(grid[i][j]==1){
+                    cntFresh++;
+                }
+            }
+        }
+        
+        int tm=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            maxi=max(maxi,t);
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                
+                if(nrow>=0 &&nrow<n &&ncol>=0 &&ncol<m &&vis[nrow][ncol]==0 &&grid[nrow][ncol]==1)
+                {
+                q.push({{nrow,ncol},t+1});
+                vis[nrow][ncol]=2;
+                cnt++;
+                }
+            }
+        }
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         if(vis[i][j]!=2 &&grid[i][j]==1) return -1;
+        //     }
+        // }
+        if(cnt!=cntFresh) return -1;
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
---
+++ DS/GRAPH/CPP/IMPLEMENTATOIN/RAJ/gp_08_(bfs)_detect_cycle_in_undirected_graph.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    bool detect(int src,vector<int>adj[],vector<int>&vis){
+        vis[src]=1;
+        queue<pair<int,int>>q;
+        q.push({src,-1});
+        while(!q.empty())
+        {
+            int node=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            
+            for(auto adjN:adj[node]){
+                if(!vis[adjN]){
+                    vis[adjN]=1;
+                    q.push({adjN,node});
+                }
+                else if(parent!=adjN){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        
+        vector<int>vis(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i]){
+                if(detect(i,adj,vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+      
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+
+
+// TIME COMPLEXITY: O(V+2E)
+// SPACE COMPLEXITY: O(V)
