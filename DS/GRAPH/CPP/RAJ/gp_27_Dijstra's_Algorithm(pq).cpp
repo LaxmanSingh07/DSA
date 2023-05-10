fde2ff08546b23313e80235096a1d312924889ab
LAXMAN <laxsingh9659@gmail.com>
2023-05-10 20:44:09 +0530

    Some Graph

---
+++ DS/GRAPH/CPP/RAJ/gp_27_Dijstra's_Algorithm(pq).cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex S.
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> dist(V);
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = 1e9;
+        }
+        dist[S] = 0;
+        pq.push({0, S});
+
+        while (!pq.empty())
+        {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node])
+            {
+                int edgeWeight = it[1];
+                int adjNode = it[0];
+
+                if (dis + edgeWeight < dist[adjNode])
+                {
+                    dist[adjNode] = dis + edgeWeight;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i = 0;
+        while (i++ < E)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1, t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin >> S;
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(V, adj, S);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// Dijkstra’s algorithm is a Greedy algorithm and time complexity is O(VLogV) (with the use of Fibonacci heap). Dijkstra doesn’t work for Graphs with negative weight edges, Bellman-Ford works for such graphs. Dijkstra’s algorithm requires the graph to be non-negative. This algorithm can be implemented with priority_queue in STL which is implemented using Heap data structure which in turn is implemented using balanced BSTs.
+
+// The reason why Dijsktra's algo doesn't work with negative weight because it will fall in an infinite loop
---
+++ DS/GRAPH/CPP/RAJ/gp_27_Dijstra's_Algorithm(set).cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex S.
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        set<pair<int, int>> st;
+
+        vector<int> dist(V, 1e9);
+
+        st.insert({0, S});
+        dist[S] = 0;
+
+        while (!st.empty())
+        {
+            auto it = *(st.begin());
+            int node = it.second;
+            int dis = it.first;
+            st.erase(it);
+
+            for (auto it : adj[node])
+            {
+                int adjNode = it[0];
+                int edgW = it[1];
+
+                if (dis + edgW < dist[adjNode])
+                {
+                    // erase if it exists
+
+                    if (dist[adjNode] != 1e9)
+                    {
+                        st.erase({dist[adjNode], adjNode});
+                    }
+
+                    dist[adjNode] = dis + edgW;
+                    st.insert({dist[adjNode], adjNode});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i = 0;
+        while (i++ < E)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1, t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin >> S;
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(V, adj, S);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
